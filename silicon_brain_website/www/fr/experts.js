17acbccf873a0271a6f88e897a2f0af3700beff3
P-Godfroid <pierre.godfroid@silicon-brain.com>
2024-12-18 15:22:47 +0100

    fix(site): experts fetch from doctype

--- silicon_brain_website/www/fr/experts.js
+++ silicon_brain_website/www/fr/experts.js
@@ -46,82 +46,85 @@ function trigger_opacity(){
 
   var resellers=[];
   //list of resellers
-  
-  frappe.call({
-    method:"silicon_brain_website.silicon_brain_website.doctype.ioi_site_reseller.ioi_site_reseller.get_resellers",
-    async: false,
-    callback: function (r) {
-      var locator_list= document.getElementById("locator_list");
-      var reseller_card_list=document.getElementById("reseller_card_list");
-      var reseller_list=document.getElementById("reseller_list");
 
-      var used_resellers = [];
-      resellers=r.message;
-      
-      
-        for (let i = resellers.length - 1; i >= 0; i--) {
-            
-            //map link
-            var elementli = document.createElement("li");
-            let mainhtmlmap=""
-            if(resellers[i].email !== ""){
-                mainhtmlmap = '<br><a href="mailto:' + resellers[i].email + '">' + resellers[i].email + '</a>'
-            }
-            let websitehtmlleft=""
-            if(resellers[i].website){
-              websitehtmlleft = '<a href="' + resellers[i].website + '" target="_blank" rel="noopener">' + resellers[i].vweb + '</a>';
-          }
-            elementli.className="locatorList__listEl | location";
-            elementli.innerHTML = '<div class="resellers-all "><div class="resellers-img pc-only"><div id="resellers_img"><img src='+ resellers[i].image +' loading="lazy" onclick="show_nav(this)" data-nav='+ resellers[i].nav_id +' class=" referencesSlider__icon | db | fit oc" alt='+ resellers[i].full_name +`></div><div style="margin-right:auto; margin-left:auto; padding-top:15px; min-width:50%"><a href="https://erp.sales.ioi.online/?var=`+resellers[i].identification.toUpperCase()+`#signup" onclick="gtag_report_conversion('https://erp.sales.ioi.online/?var=`+resellers[i].identification.toUpperCase()+`#signup', 'AW-11139658231/pOHSCIu8o5YYEPfj5r8p' ,11820690)" `+'><button class="btn btn--s" style="top:50%;" type=submit><span class="btn__label">Essai gratuit / Commande</span><span class="btn__bg | fill"></span><svg class="m-only | nlForm__submitIcon | icon icon--arrow" ``viewBox="0 0 35 32" aria-hidden="true" fill="none" stroke-width="4" stroke="currentColor"><path d="M0 16h32" /><path d="m18 2 14 14-14 14" /></svg></button></a></div></div><div class="resellers-txt"><a onclick="map_focus(this)" data-lon=' + resellers[i].longitude + ' data-lat=' + resellers[i].latitude + ' class="pointer"><h3 class="location__title | title | fw-bold">' + resellers[i].full_name + '<br></h3><address class="location__address">' + resellers[i].address + '</address></a><p>' + resellers[i].phone  + mainhtmlmap + '<br>'+websitehtmlleft+`</p></div><div class="mb-only"><div style="margin-right:auto; margin-left:auto; padding-top:15px; min-width:50%"><a href="https://erp.sales.ioi.online/?var=`+resellers[i].identification.toUpperCase()+`#signup" onclick="gtag_report_conversion('https://erp.sales.ioi.online/?var=`+resellers[i].identification.toUpperCase()+`#signup', 'AW-11139658231/pOHSCIu8o5YYEPfj5r8p' ,11820690)" `+'><button class="btn btn--s" style="top:50%;" type=submit><span class="btn__label">Essai gratuit / Commande</span><span class="btn__bg | fill"></span><svg class="m-only | nlForm__submitIcon | icon icon--arrow" ``viewBox="0 0 35 32" aria-hidden="true" fill="none" stroke-width="4" stroke="currentColor"><path d="M0 16h32" /><path d="m18 2 14 14-14 14" /></svg></button></a></div></div></div>';
-            locator_list.appendChild(elementli);
+  window.onload = function () {
   
-            if(!used_resellers.includes(resellers[i].short_id)){
-  
-            //card list
-            var elementdiv = document.createElement("div");
-            elementdiv.className="referencesSlider__slide | actr pr | col-4 t-col-4 | m-100 | js-SmoothSlider-slide";
-            elementdiv.innerHTML = '<figure class="referencesSlider__fig | up fit"><img src='+ resellers[i].image +' loading="lazy" onclick="show_nav(this)" data-nav='+ resellers[i].nav_id +' class=" referencesSlider__icon | db | fit oc" alt='+ resellers[i].full_name +'></figure><a href="#" target="_blank" rel="noopener" class="fill"><span class="sr-only">Want to know more ?</span></a>'
-            reseller_card_list.appendChild(elementdiv);
-  
-            //nav list
-            let navhtml = '';
-            let emailhtml = "";
-            let websitehtml="";
-            for (let j = resellers.length - 1; j >= 0; j--) {
-                if(resellers[i].short_id == resellers[j].short_id){
-                      if(resellers[j].mail){
-                          emailhtml = '<br><a href="mailto:' + resellers[j].email + '">' + resellers[j].email + '</a>'
-                      }
-                      if(resellers[j].website){
-                          websitehtml = '<a href="' + resellers[j].website + '" target="_blank" rel="noopener">' + resellers[j].vweb + '</a>';
-                      }
-                    if (navhtml === ''){
-                        navhtml = navhtml + '<div class="flex_change nav_txt" style="display:flex; flex-direction: row;"><div style="min-width:50%; padding-left:25px;">'
-                        navhtml = navhtml + '<p class="nav-text-modif nav-text">'+ resellers[j].full_name +'</p><p class="nav-text">'+ resellers[j].address +'<br></p>' + '<p class="nav-text">' + resellers[j].phone + emailhtml + '<br>'+websitehtml+'</p>';
-                    }else{
-                        navhtml = navhtml + '<div class="flex_change nav_txt" style="display:flex; flex-direction: row;"><div style="min-width:50%; padding-left:25px;">'
-                        navhtml = navhtml + '<br><p class="nav-text-modif nav-text">'+ resellers[j].full_name +'</p><p class="nav-text">'+ resellers[j].address +'<br></p>' + '<p class="nav-text">' + resellers[j].phone + emailhtml + '<br>'+websitehtml+'</p>';
-                    }
-  
-                    navhtml = navhtml + `</div><div style="margin-right:auto; margin-left:auto; padding-top:15px; min-width:50%"><a href="https://erp.sales.ioi.online/?var=`+resellers[i].identification.toUpperCase()+`#signup" onclick="gtag_report_conversion('https://erp.sales.ioi.online/?var=`+resellers[i].identification.toUpperCase()+`#signup', 'AW-11139658231/pOHSCIu8o5YYEPfj5r8p' ,11820690)`+'"><button class="btn btn--s" style="top:50%;" type=submit><span class="btn__label">Essai gratuit / Commande</span><span class="btn__bg | fill"></span><svg class="m-only | nlForm__submitIcon | icon icon--arrow" ``viewBox="0 0 35 32" aria-hidden="true" fill="none" stroke-width="4" stroke="currentColor"><path d="M0 16h32" /><path d="m18 2 14 14-14 14" /></svg></button></a></div></div>'
+    frappe.call({
+        method:"silicon_brain_website.silicon_brain_website.doctype.ioi_site_reseller.ioi_site_reseller.get_site_resellers",
+        async: false,
+        callback: function (r) {
+        var locator_list= document.getElementById("locator_list");
+        var reseller_card_list=document.getElementById("reseller_card_list");
+        var reseller_list=document.getElementById("reseller_list");
+
+        var used_resellers = [];
+        resellers=r.message;
+        
+        
+            for (let i = resellers.length - 1; i >= 0; i--) {
+                
+                //map link
+                var elementli = document.createElement("li");
+                let mainhtmlmap=""
+                if(resellers[i].email !== ""){
+                    mainhtmlmap = '<br><a href="mailto:' + resellers[i].email + '">' + resellers[i].email + '</a>'
                 }
+                let websitehtmlleft=""
+                if(resellers[i].website){
+                websitehtmlleft = '<a href="' + resellers[i].website + '" target="_blank" rel="noopener">' + resellers[i].vweb + '</a>';
             }
-            var elementnav = document.createElement("nav");
-            elementnav.setAttribute('id', resellers[i].nav_id);
-            elementnav.className="brainstorming-nav nav-all hidden_nav";
-            let summary = ""
-            if(resellers[i].summary !== ""){
-              summary = '</div><div class="summary-txt"><p>'+ resellers[i].summary +'</p></div>';
-              }
-            elementnav.innerHTML = '<div class="nav-header"><a href='+ resellers[i].website +'><img class="nav-img" src='+ resellers[i].image +' loading="lazy" alt="'+ resellers[i].full_name +'"/> </a>' + summary + navhtml;
-            reseller_list.appendChild(elementnav);
-  
+                elementli.className="locatorList__listEl | location";
+                elementli.innerHTML = '<div class="resellers-all "><div class="resellers-img pc-only"><div id="resellers_img"><img src='+ resellers[i].image +' loading="lazy" onclick="show_nav(this)" data-nav='+ resellers[i].nav_id +' class=" referencesSlider__icon | db | fit oc" alt='+ resellers[i].full_name +`></div><div style="margin-right:auto; margin-left:auto; padding-top:15px; min-width:50%"><a href="https://erp.sales.ioi.online/?var=`+resellers[i].identification.toUpperCase()+`#signup" onclick="gtag_report_conversion('https://erp.sales.ioi.online/?var=`+resellers[i].identification.toUpperCase()+`#signup', 'AW-11139658231/pOHSCIu8o5YYEPfj5r8p' ,11820690)" `+'><button class="btn btn--s" style="top:50%;" type=submit><span class="btn__label">Essai gratuit / Commande</span><span class="btn__bg | fill"></span><svg class="m-only | nlForm__submitIcon | icon icon--arrow" ``viewBox="0 0 35 32" aria-hidden="true" fill="none" stroke-width="4" stroke="currentColor"><path d="M0 16h32" /><path d="m18 2 14 14-14 14" /></svg></button></a></div></div><div class="resellers-txt"><a onclick="map_focus(this)" data-lon=' + resellers[i].longitude + ' data-lat=' + resellers[i].latitude + ' class="pointer"><h3 class="location__title | title | fw-bold">' + resellers[i].full_name + '<br></h3><address class="location__address">' + resellers[i].address + '</address></a><p>' + resellers[i].phone  + mainhtmlmap + '<br>'+websitehtmlleft+`</p></div><div class="mb-only"><div style="margin-right:auto; margin-left:auto; padding-top:15px; min-width:50%"><a href="https://erp.sales.ioi.online/?var=`+resellers[i].identification.toUpperCase()+`#signup" onclick="gtag_report_conversion('https://erp.sales.ioi.online/?var=`+resellers[i].identification.toUpperCase()+`#signup', 'AW-11139658231/pOHSCIu8o5YYEPfj5r8p' ,11820690)" `+'><button class="btn btn--s" style="top:50%;" type=submit><span class="btn__label">Essai gratuit / Commande</span><span class="btn__bg | fill"></span><svg class="m-only | nlForm__submitIcon | icon icon--arrow" ``viewBox="0 0 35 32" aria-hidden="true" fill="none" stroke-width="4" stroke="currentColor"><path d="M0 16h32" /><path d="m18 2 14 14-14 14" /></svg></button></a></div></div></div>';
+                locator_list.appendChild(elementli);
+    
+                if(!used_resellers.includes(resellers[i].short_id)){
+    
+                //card list
+                var elementdiv = document.createElement("div");
+                elementdiv.className="referencesSlider__slide | actr pr | col-4 t-col-4 | m-100 | js-SmoothSlider-slide";
+                elementdiv.innerHTML = '<figure class="referencesSlider__fig | up fit"><img src='+ resellers[i].image +' loading="lazy" onclick="show_nav(this)" data-nav='+ resellers[i].nav_id +' class=" referencesSlider__icon | db | fit oc" alt='+ resellers[i].full_name +'></figure><a href="#" target="_blank" rel="noopener" class="fill"><span class="sr-only">Want to know more ?</span></a>'
+                reseller_card_list.appendChild(elementdiv);
+    
+                //nav list
+                let navhtml = '';
+                let emailhtml = "";
+                let websitehtml="";
+                for (let j = resellers.length - 1; j >= 0; j--) {
+                    if(resellers[i].short_id == resellers[j].short_id){
+                        if(resellers[j].mail){
+                            emailhtml = '<br><a href="mailto:' + resellers[j].email + '">' + resellers[j].email + '</a>'
+                        }
+                        if(resellers[j].website){
+                            websitehtml = '<a href="' + resellers[j].website + '" target="_blank" rel="noopener">' + resellers[j].vweb + '</a>';
+                        }
+                        if (navhtml === ''){
+                            navhtml = navhtml + '<div class="flex_change nav_txt" style="display:flex; flex-direction: row;"><div style="min-width:50%; padding-left:25px;">'
+                            navhtml = navhtml + '<p class="nav-text-modif nav-text">'+ resellers[j].full_name +'</p><p class="nav-text">'+ resellers[j].address +'<br></p>' + '<p class="nav-text">' + resellers[j].phone + emailhtml + '<br>'+websitehtml+'</p>';
+                        }else{
+                            navhtml = navhtml + '<div class="flex_change nav_txt" style="display:flex; flex-direction: row;"><div style="min-width:50%; padding-left:25px;">'
+                            navhtml = navhtml + '<br><p class="nav-text-modif nav-text">'+ resellers[j].full_name +'</p><p class="nav-text">'+ resellers[j].address +'<br></p>' + '<p class="nav-text">' + resellers[j].phone + emailhtml + '<br>'+websitehtml+'</p>';
+                        }
+    
+                        navhtml = navhtml + `</div><div style="margin-right:auto; margin-left:auto; padding-top:15px; min-width:50%"><a href="https://erp.sales.ioi.online/?var=`+resellers[i].identification.toUpperCase()+`#signup" onclick="gtag_report_conversion('https://erp.sales.ioi.online/?var=`+resellers[i].identification.toUpperCase()+`#signup', 'AW-11139658231/pOHSCIu8o5YYEPfj5r8p' ,11820690)`+'"><button class="btn btn--s" style="top:50%;" type=submit><span class="btn__label">Essai gratuit / Commande</span><span class="btn__bg | fill"></span><svg class="m-only | nlForm__submitIcon | icon icon--arrow" ``viewBox="0 0 35 32" aria-hidden="true" fill="none" stroke-width="4" stroke="currentColor"><path d="M0 16h32" /><path d="m18 2 14 14-14 14" /></svg></button></a></div></div>'
+                    }
+                }
+                var elementnav = document.createElement("nav");
+                elementnav.setAttribute('id', resellers[i].nav_id);
+                elementnav.className="brainstorming-nav nav-all hidden_nav";
+                let summary = ""
+                if(resellers[i].summary !== ""){
+                summary = '</div><div class="summary-txt"><p>'+ resellers[i].summary +'</p></div>';
+                }
+                elementnav.innerHTML = '<div class="nav-header"><a href='+ resellers[i].website +'><img class="nav-img" src='+ resellers[i].image +' loading="lazy" alt="'+ resellers[i].full_name +'"/> </a>' + summary + navhtml;
+                reseller_list.appendChild(elementnav);
+    
+                }
+    
+                used_resellers.push(resellers[i].short_id);
             }
-  
-            used_resellers.push(resellers[i].short_id);
         }
-    }
-});
+    });
+}
 
     
     
