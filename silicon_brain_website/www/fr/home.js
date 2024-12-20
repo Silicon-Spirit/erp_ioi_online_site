865c0ca9fd03e49d8f343eeefae1f0cc0bd30732
P-Godfroid <pierre.godfroid@silicon-brain.com>
2024-12-20 10:48:55 +0100

    fix(site): js and css recovered

--- silicon_brain_website/www/fr/home.js
+++ silicon_brain_website/www/fr/home.js
@@ -9,16 +9,13 @@ window.onload = function () {
 				number:4
 			},
 			callback: function (r) {
-				console.log("callback")
 				if (r.message)
 				{
-					console.log(r.message)
 					let reseller={}
 					let four_resellers=four_resellers_el.children
 					for (var i = 0; i < four_resellers.length; i++) {
 						reseller=r.message[i]
-						console.log()
-						four_resellers[i].innerHTML = `<div class="resellers-img"><div id="resellers_img"><img src="${reseller.image}" loading="lazy" data-nav=${reseller.nav_id } class=" referencesSlider__icon | db | fit oc" alt=${reseller.full_name}></div><div style="margin-right:auto; margin-left:auto; padding-top:15px; min-width:50%"></div></div><div class="resellers-txt"><h3 class="location__title | title | fw-bold">${reseller.full_name}<br></h3><address class="location__address">${reseller.address}</address><p>${reseller.phone}${reseller.email?'<br><a href="mailto:' + reseller.email + '">' + reseller.email + '</a>':""}<br>${reseller.website?'<a href="' + reseller.website + '" target="_blank" rel="noopener">' + reseller.website.replace("https://","") + '</a>':""}</p></div></div>`;
+						four_resellers[i].innerHTML = `<div class="resellers-img"><div id="resellers_img"><img src="${reseller.image}" loading="lazy" data-nav=${reseller.nav_id } class=" referencesSlider__icon | db | fit oc" alt=${reseller.full_name}></div><div style="margin-right:auto; margin-left:auto; padding-top:15px; min-width:50%"></div></div><div class="resellers-txt"><h3 class="location__title | title | fw-bold">${reseller.full_name}</h3><p>${reseller.phone}${reseller.email?'<br><a href="mailto:' + reseller.email + '">' + reseller.email + '</a>':""}<br>${reseller.website?'<a href="' + reseller.website + '" target="_blank" rel="noopener">' + reseller.website.replace("https://","") + '</a>':""}</p></div></div><a href="https://erp.sales.ioi.online/?var=${reseller.identification}#signup" onclick="gtag_report_conversion('https://erp.sales.ioi.online/?var=${reseller.identification}#signup', 'AW-11139658231/pOHSCIu8o5YYEPfj5r8p' ,11820690)"><button class="btn btn--s" type="submit"><span class="btn__label">Essai gratuit / Commande</span><span class="btn__bg | fill"></span><svg class="m-only | nlForm__submitIcon | icon icon--arrow" viewBox="0 0 35 32" aria-hidden="true" fill="none" stroke-width="4" stroke="currentColor"><path d="M0 16h32"></path><path d="m18 2 14 14-14 14"></path></svg></button></a>`;
 					}
 				}
 				
